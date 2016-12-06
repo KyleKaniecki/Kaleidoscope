2c58dd7e4aa8ed996edd5d5f5974a40aed3a2762
Kyle Kaniecki <kyle.kaniecki@ymail.com>
2016-12-05 19:14:39 -0500

    Initial models.py

---
+++ accounts/models.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib.auth import (authenticate,
+                                get_user_model,
+                                login,
+                                logout)
+import re
+
+# Create your models here.
+
