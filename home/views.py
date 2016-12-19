f4b1e2d3d766157c4fe5f415cc7100d017815d43
KyleKaniecki <kyle.kaniecki@ymail.com>
2016-12-19 08:13:09 -0500

    Redid homepage with isotope + packery instead of just packery

--- home/views.py
+++ home/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render,redirect
 
 from django.views.generic import View
 
+from listings.models import Listing
+
 
 class Home(View):
 
     def get(self,request):
-        return render(request,"home/homepage.html")
+        listings = Listing.objects.all()
+        return render(request,"home/homepage.html",{"listings": listings})
